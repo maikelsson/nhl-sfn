105d5adb0b8c16e588b818512e38acaf9d5a74ac
mikael thornberg <mikael.thornberg@hotmail.com>
2021-03-24 09:49:57 +0200

    vercel changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import app from "./app";
+import dotenv from "dotenv";
+import { resolve } from "path";
 
-const PORT = 4000;
+dotenv.config({ path: resolve(__dirname, "../.env") });
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
