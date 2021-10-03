77a97bc4b31040afacc721e3170c55e4b6c826c0
mikael thornberg <mikael.thornberg@hotmail.com>
2021-10-03 09:13:38 +0300

    stuff

--- client/src/components/game/GameStatus.tsx
+++ client/src/components/game/GameStatus.tsx
@@ -15,25 +15,37 @@ export const GameStatus = (props: Props) => {
       minutes = from.getMinutes().toString();
     }
 
-    const result: string = `${from.getHours() < 10 ? "0" : ""}${from.getHours()}:${minutes}`;
+    const result: string = `${
+      from.getHours() < 10 ? "0" : ""
+    }${from.getHours()}:${minutes}`;
 
     return result;
   };
 
   return (
     <div className="flex items-center justify-between w-40 font-medium">
-      <h1 className={`text-2xl ${props.info.homeScore > props.info.awayScore ? "" : "text-gray-400"}`}>
+      <h1
+        className={`text-2xl ${
+          props.info.homeScore > props.info.awayScore ? "" : "text-gray-400"
+        }`}
+      >
         {props.info.homeScore}
       </h1>
       <div className="flex flex-col justify-center items-center">
         <h4 className="">{props.info.status}</h4>
         {props.info.status !== "Final" ? (
-          <h5 className="font-normal text-gray-400">{displayHours(props.info.startTime)}</h5>
+          <h5 className="font-normal text-gray-400">
+            {displayHours(props.info.startTime)}
+          </h5>
         ) : (
           <></>
         )}
       </div>
-      <h1 className={`text-2xl ${props.info.awayScore > props.info.homeScore ? "" : "text-gray-400"}`}>
+      <h1
+        className={`text-2xl ${
+          props.info.awayScore > props.info.homeScore ? "" : "text-gray-400"
+        }`}
+      >
         {props.info.awayScore}
       </h1>
     </div>
